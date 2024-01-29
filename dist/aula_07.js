c57c990f9f50bfc1a5bd41cb7460c54184ebeb12
João Claudio Prestes <119965861+joaoclaudioprestes@users.noreply.github.com>
2024-01-29 20:55:55 -0300

    new class tuplas

---
+++ dist/aula_07.js
@@ -0,0 +1,3 @@
+"use strict";
+let coisas = [12, 43, 534];
+let tuplasCoisas = ["João", 19, true]; // precisamos especificar cada dado presente na tupla
---
+++ src/aula_07.ts
@@ -0,0 +1,3 @@
+let coisas: (string | number)[] = [12, 43, 534];
+
+let tuplasCoisas: [string, number, boolean] = ["João", 19, true]; // precisamos especificar cada dado presente na tupla
